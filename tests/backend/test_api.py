b57c27034523b449254409ef0ce9fb4934a32865
AlinaProkofeva <connect-trener@mail.ru>
2024-01-21 23:26:59 +0300

    add test reset & confirm reset password

--- tests/backend/test_api.py
+++ tests/backend/test_api.py
@@ -1,8 +1,9 @@
 import random
 from unittest.mock import patch
-
 import pytest
+from django.contrib.auth.hashers import check_password
 from django.urls import reverse
+from django_rest_passwordreset.models import ResetPasswordToken
 from model_bakery import baker
 
 from backend.models import Shop, User, ConfirmEmailToken
@@ -320,3 +321,56 @@ def test_confirm_account(client_pytest, email, correctness, exp_res, exp_active,
     data = res.json()
     if exp_res != 200:
         assert data == error_text
+
+
+@patch.object(task_send_email, 'delay')
+@pytest.mark.parametrize(
+    ['email', 'arg', 'value', 'exp_res'],
+    (
+            ('any@m.ru', 'email', 'any@m.ru', 200),         # все ОК
+            ('any@m.ru', 'not_email', 'any@m.ru', 400)      # нет почты в data
+    )
+)
+@pytest.mark.django_db
+def test_reset_password_request(mock_delay, client_pytest, email, arg, value, exp_res):
+    """Проверяем запрос сброса пароля, что создается токен на сброс"""
+
+    user = User.objects.create(is_active=True, email=email, password='1234Qwerty!!!')
+    post_data = {arg: value}
+    res = client_pytest.post(reverse('password_reset'), post_data)
+    assert res.status_code == exp_res
+
+    if res.status_code == 200:
+        assert ResetPasswordToken.objects.filter(user=user).exists() is True
+
+
+@patch.object(task_send_email, 'delay')
+@pytest.mark.parametrize(
+    ['email', 'arg_1', 'value_1', 'arg_2', 'value_2', 'exp_res'],
+    (
+            ('any@m.ru', 'password', 'newpass1234!!!', 'token', 'TO_UPD', 200),    # все ОК (переопределить токен!)
+            ('any@m.ru', 'password', 'newpass1234!!!', 'token', 'qwe345', 404),    # неверный токен
+            ('any@m.ru', 'passwor', 'newpass1234!!!', 'token', 'TO_UPD', 400),     # нет пароля
+            ('any@m.ru', 'password', 'newpass1234!!!', 'toke', 'qqq111', 400),     # нет токена
+            ('any@m.ru', 'password', '1234', 'token', 'TO_UPD', 400)               # некорректный пароль
+    )
+)
+@pytest.mark.django_db
+def test_reset_password_confirm(mock_delay, client_pytest, email, arg_1, value_1, arg_2, value_2, exp_res):
+    """Проверяем сброс и смену пароля по токену"""
+
+    user = User.objects.create(is_active=True, email=email, password='1234Qwerty!!!')
+    client_pytest.post(reverse('password_reset'), {'email': email})
+
+    if value_2 == 'TO_UPD':
+        value_2 = ResetPasswordToken.objects.get(user=user).key
+    post_data = {arg_1: value_1, arg_2: value_2}
+
+    res = client_pytest.post(reverse('password_reset_confirm'), post_data)
+    assert res.status_code == exp_res
+
+    # проверяем смену пароля на новый
+    if exp_res == 200:
+        passw = User.objects.get(email=email).password
+        check = check_password(value_1, passw)
+        assert check is True
